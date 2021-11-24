655ca9802ece476254fb0b801de650b6c6bae334
Mayariff <79027972026@yandex.ru>
2021-11-24 11:08:40 +0500

    init commit

---
+++ src/1_main/m1-UI/common/c5-SuperSelect/SuperSelect.tsx
@@ -0,0 +1,37 @@
+import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react'
+import s from './SuperSelect.module.css'
+
+type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
+
+type SuperSelectPropsType = DefaultSelectPropsType & {
+    options?: any[]
+    onChangeOption?: (option: any) => void
+}
+
+const SuperSelect: React.FC<SuperSelectPropsType> = (
+    {
+        options,
+        onChange, onChangeOption,
+        ...restProps
+    }
+) => {
+
+    const mappedOptions: any[] = options ? options.map((o, i) => (
+        <option key={i} value={o} className={s.option}>{o}</option>
+    )) : []
+
+
+    const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
+    }
+
+    return (<div className={s.wrapper}>
+            <select onChange={onChangeCallback} {...restProps} className={s.select}>
+                {mappedOptions}
+            </select>
+    </div>
+    )
+}
+
+export default SuperSelect
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './1_main/m1-UI/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
